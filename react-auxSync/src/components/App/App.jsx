eda890205cc20c999d2d317055b8f02124a1e2d1
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-02-07 22:01:32 -0600

    Added user and route foundation

--- react-auxSync/src/components/App/App.jsx
+++ react-auxSync/src/components/App/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 //components
 import { Header } from "../Header/Header";
 import { Main } from "../Main/Main";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleUserChange = (e) => {
+    e.preventDefault();
+    setUser(e.target.value);
+    console.log(user);
+  };
+
   return (
     <div className="page">
       <div className="page__content">
         <div className="falling-gifs">
           <Header />
-          <Main />
+          <Routes>
+            <Route path="/" element={<Main handleUserChange={handleUserChange} user={user} />} />
+          </Routes>
         </div>
       </div>
     </div>
--- react-auxSync/src/components/Main/Main.jsx
+++ react-auxSync/src/components/Main/Main.jsx
@@ -2,13 +2,20 @@ import "./Main.css";
 import { Options } from "../Options/Options.jsx";
 import { Instructions } from "../Instructions/Instructions.jsx";
 
-export function Main() {
+export function Main({ handleUserChange, user }) {
   return (
     <main className="main">
       <div className="main__content">
         <Options />
         <form className="nickname-form">
-          <input className="nickname_input" placeholder="Nickname here" minLength="2" maxLength="20" />
+          <input
+            className="nickname_input"
+            placeholder="Nickname here"
+            minLength="2"
+            maxLength="20"
+            onChange={handleUserChange}
+            value={user}
+          />
         </form>
         <Instructions />
       </div>
