693707dde01c2f49d35e7cc6fbab151a490bc4e6
Ken Garcia <142529062+Sourcloudii@users.noreply.github.com>
2026-02-09 08:15:35 +0000

    Codespace commits - api integration

--- react-auxSync/src/components/App/App.jsx
+++ react-auxSync/src/components/App/App.jsx
@@ -1,27 +1,41 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 //components
 import { Header } from "../Header/Header";
 import { Main } from "../Main/Main";
 
+import { apiKey } from "../../utils/constants";
+import { getGifs } from "../../utils/giphyApi";
+
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState("");
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    getGifs({ apiKey }).then((e) => {
+      setGifs(e.data);
+      console.log(e.data);
+    });
+  }, []);
 
   const handleUserChange = (e) => {
     e.preventDefault();
     setUser(e.target.value);
-    console.log(user);
   };
 
   return (
     <div className="page">
       <div className="page__content">
         <div className="falling-gifs">
+          {gifs.map((gifs) => {
+          <img src={gifs.images.original.url} alt={`${gifs.title}`} className="falling-gifs_imgs" />
+          })}
           <Header />
           <Routes>
             <Route path="/" element={<Main handleUserChange={handleUserChange} user={user} />} />
+            <Route path="/host-page" /*element={ }*/ />
           </Routes>
         </div>
       </div>
--- react-auxSync/src/utils/constants.js
+++ react-auxSync/src/utils/constants.js
@@ -1,6 +1,18 @@
+export const apiKey = "WalJLzD6rC1dYiLFNsWW7rcrZBvaAem0";
+
 export const instructions = [
   "Choose a Gif for your friends to battle with",
   "Go against friends to sync the perfect song to the Gif.",
   "Vote for the song that syncs with the GIF the best",
   "Take control of the Aux crown when you triumph over everyone",
+];
+
+export const quotes = [
+  "No Aux battle was ever won according to plan, but no Aux battle was ever won without one.",
+  "The rush of AuxBattle is often a potent and lethal addiction, for music is a drug.",
+];
+
+export const quoteAuthors = [
+  "Dwight D. Auxenhower",
+  "Chris Hedgaux",
 ];
