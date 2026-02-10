9c95a051d8284d7bf8075519ee675aab68d75461
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-02-09 23:47:49 -0600

    Prettier and rate limit changed

--- react-auxSync/src/utils/giphyApi.js
+++ react-auxSync/src/utils/giphyApi.js
@@ -1,5 +1,7 @@
-import { checkResponse } from "./api"
+import { checkResponse } from "./api";
 
 export const getTrendingGifs = ({ apiKey }) => {
-    return fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25&country_code=us&remove_low_contrast=true`).then(checkResponse);
-}
+  return fetch(
+    `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=50&country_code=us&remove_low_contrast=true`,
+  ).then(checkResponse);
+};
