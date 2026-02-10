6f2b9b09d640029d0042a45a55735f2cd80ecffb
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-02-09 23:44:09 -0600

    Added quotes for aux battling

--- react-auxSync/src/utils/constants.js
+++ react-auxSync/src/utils/constants.js
@@ -8,11 +8,48 @@ export const instructions = [
 ];
 
 export const quotes = [
-  "No Aux battle was ever won according to plan, but no Aux battle was ever won without one.",
-  "The rush of AuxBattle is often a potent and lethal addiction, for music is a drug.",
-];
-
-export const quoteAuthors = [
-  "Dwight D. Auxenhower",
-  "Chris Hedgaux",
+  {
+    quote: "No Aux battle was ever won according to plan, but no Aux battle was ever won without one.",
+    author: "Dwight D. Auxenhower",
+  },
+  {
+    quote: "The rush of AuxBattle is often a potent and lethal addiction, for music is a drug.",
+    author: "Chris Hedgaux",
+  },
+  {
+    quote: "Music is the battlefield of emotions.",
+    author: "Auxie",
+  },
+  {
+    quote: "In Aux battle, there are no unwounded soldiers.",
+    author: "José Auxorosky",
+  },
+  {
+    quote: "Only the best have seen the end of aux battle.",
+    author: "Auxto",
+  },
+  {
+    quote: "Aux Battles do not determine who is right — only who is left.",
+    author: "Auxtrand Russell",
+  },
+  {
+    quote: "The supreme art of aux battle is to subdue the enemy without fighting.",
+    author: "Aux Tzu",
+  },
+  {
+    quote: "Every Aux battle is won before it's ever fought.",
+    author: "Aux Tzu",
+  },
+  {
+    quote: "You may have to fight an Aux battle more than once to win it.",
+    author: "Auxgret Thatcher",
+  },
+  {
+    quote: "Gifs in life are intended to make us better, not bitter.",
+    author: "Dan Raux",
+  },
+  {
+    quote: "Auxing isn't something you do, it's something you live.",
+    author: "Aux-One",
+  },
 ];
