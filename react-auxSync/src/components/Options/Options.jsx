d8f8a0516d51e4acb7033b981c3a0fdd33902cb6
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-02-13 04:04:04 -0600

    Join modal functionality

--- react-auxSync/src/components/Options/Options.jsx
+++ react-auxSync/src/components/Options/Options.jsx
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 import auxCord from "../../images/aux-cord.svg";
 import bluetooth from "../../images/bluetooth.svg";
 
-export function Options() {
+export function Options({ openJoinModal }) {
   return (
     <div className="options">
       <div className="options__content">
         <nav className="options__nav">
           <div className="btn-wrapper">
             <img src={auxCord} alt="Aux Cord" className="aux-cord-img" />
-            <Link to="/join-page" className="options__link">
-              <button className="options_btn options-join__link">Join</button>
-            </Link>
+            <button
+              className="options_btn options-join__link"
+              onClick={openJoinModal}
+            >
+              Join
+            </button>
           </div>
           <div className="btn-wrapper">
             <img src={bluetooth} alt="Bluetooth" className="bluetooth-img" />
