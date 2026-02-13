2431c7fa791b12c41f182a390c3cb24ab5c5b1dc
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-02-13 04:05:23 -0600

    Header syntax for different pages

--- react-auxSync/src/components/Header/Header.jsx
+++ react-auxSync/src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 import title from "../../images/title.svg";
 
-export function Header() {
-  return (
+export function Header({ location }) {
+  return location === "/" ? (
     <header className="header">
       <div className="header__content">
         <Link to="/">
@@ -11,5 +11,13 @@ export function Header() {
         </Link>
       </div>
     </header>
-  );
+  ) : location === "/host-page" || location === "/lobby-page" ? (
+    <header className="header">
+      <div className="header__content header__content_in-game">
+        <Link to="/">
+          <img src={title} alt="AuxSync" className="header__title" />
+        </Link>
+      </div>
+    </header>
+  ) : null;
 }
