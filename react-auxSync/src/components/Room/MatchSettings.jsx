2efb33d74155cfc4aac23dea7492c7f80efd4532
Ken Garcia <142529062+Sourcloudii@users.noreply.github.com>
2026-02-14 06:27:57 +0000

    MatchSettings styling and structure

--- react-auxSync/src/components/Room/MatchSettings.jsx
+++ react-auxSync/src/components/Room/MatchSettings.jsx
@@ -3,15 +3,27 @@ import "./MatchSettings.css";
 export function MatchSettings() {
   return (
     <div className="settings_options">
-      <h2 className="settings_options__title">Match Settings</h2>
-      <button className="settings_options__lobby-size">Lobby Size</button>
-      <button className="settings_options__song-selection-time">
-        Song Selection Time
-      </button>
-      <button className="settings_options__song-length">Song Length</button>
-      <button className="settings_options__gif-options">GIF Options</button>
-      <button className="settings_options__rounds">Rounds</button>
-      <button className="settings_options__voting-time">Voting Time</button>
+      <h2 className="settings_options__title text_shadow">Match Settings</h2>
+      <div className="settings_options-btn__wrapper">
+        <button className="settings_options-btn settings_options__lobby-size text_shadow">
+          Lobby Size
+        </button>
+        <button className="settings_options-btn settings_options__song-selection-time text_shadow">
+          Song Selection Time
+        </button>
+        <button className="settings_options-btn settings_options__song-length text_shadow">
+          Song Length
+        </button>
+        <button className="settings_options-btn settings_options__gif-options text_shadow">
+          GIF Options
+        </button>
+        <button className="settings_options-btn settings_options__rounds text_shadow">
+          Rounds
+        </button>
+        <button className="settings_options-btn settings_options__voting-time text_shadow">
+          Voting Time
+        </button>
+      </div>
     </div>
   );
 }
