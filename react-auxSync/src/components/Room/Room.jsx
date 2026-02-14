a0eff22cda3b8449cc998a3748bec0cebfb3ab85
Ken Garcia <142529062+Sourcloudii@users.noreply.github.com>
2026-02-14 06:28:57 +0000

    Room page structure and styled added

--- react-auxSync/src/components/Room/Room.jsx
+++ react-auxSync/src/components/Room/Room.jsx
@@ -1,12 +1,29 @@
 import "./Room.css";
+import exitButton from "../../images/exit-btn.svg";
 import { MatchSettings } from "./MatchSettings";
 
-export function Room() {
-    return (
-        <main className="host_page">
-            <div className="host_page__content">
-                <MatchSettings />
+import { Link } from "react-router-dom";
+
+//Add lobby code bubble and player list later
+export function Room({ lobbyCode }) {
+  return (
+    <main className="room">
+      <div className="room__content">
+        <div className="room__lobby-info">
+          <h2 className="room__lobby-code text_shadow">Lobby Code: 7B2W</h2>
+          <div className="room__lobby-players__wrapper">
+            <Link className="room__exit-btn" to="/">
+              <img src={exitButton} alt="Exit" />
+            </Link>
+            <div className="room__lobby-players-info">
+              <p className="room__lobby-host player-label">Host: </p>
+              <p className="room__lobby-players player-label">Players: </p>
+              {/*Add players later*/}
             </div>
-        </main>
-    )
+          </div>
+        </div>
+        <MatchSettings />
+      </div>
+    </main>
+  );
 }
