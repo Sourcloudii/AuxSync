b3564ca8276675976fb9754774a511a25f397dde
Ken Garcia <142529062+Sourcloudii@users.noreply.github.com>
2026-02-17 07:43:33 +0000

    CodeSpace commit, added hover & click for modal

--- react-auxSync/src/components/Room/Room.jsx
+++ react-auxSync/src/components/Room/Room.jsx
@@ -1,5 +1,6 @@
 import "./Room.css";
 import exitButton from "../../images/exit-btn.svg";
+// import clipBoard from "../../images/clipboard.png";
 import { MatchSettings } from "./MatchSettings";
 
 import { Link } from "react-router-dom";
@@ -10,7 +11,12 @@ export function Room({ lobbyCode }) {
     <main className="room">
       <div className="room__content">
         <div className="room__lobby-info">
-          <h2 className="room__lobby-code text_shadow">Lobby Code: 7B2W</h2>
+          <h2
+            className="room__lobby-code"
+            // style={{ cursor: `url(${clipBoard}), auto` }}
+          >
+            Lobby Code: 7B2W
+          </h2>
           <div className="room__lobby-players__wrapper">
             <Link className="room__exit-btn" to="/">
               <img src={exitButton} alt="Exit" />
