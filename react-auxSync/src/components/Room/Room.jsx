cb4efdf1cbc4f61e84fd7dacbfa3b0f579567b3c
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-03-10 01:12:51 -0500

    Room element added

--- react-auxSync/src/components/Room/Room.jsx
+++ react-auxSync/src/components/Room/Room.jsx
@@ -5,23 +5,34 @@ import { MatchSettings } from "./MatchSettings";
 import { Link } from "react-router-dom";
 
 //Add lobby code bubble and player list later
-export function Room({ lobbyCode }) {
+export function Room({ lobbyCode, host, players }) {
   return (
     <main className="room">
       <div className="room__content">
         <div className="room__lobby-info">
-          <h2
-            className="room__lobby-code text_shadow"
-          >
-            Lobby Code: 7B2W
-          </h2>
+          <button className="room__lobby-code-btn text_shadow">
+            Lobby Code: {lobbyCode}
+          </button>
           <div className="room__lobby-players-info">
-          <Link className="room__exit-btn" to="/">
-            <img src={exitButton} alt="Exit" />
-          </Link>
-            <p className="room__lobby-host player-label">Host: </p>
-            <p className="room__lobby-players player-label">Players: </p>
-            {/*Add players later*/}
+            <Link className="room__exit-btn" to="/">
+              <img src={exitButton} alt="Exit" />
+            </Link>
+            <div className="room__lobby-wrapper room__lobby-host-wrapper">
+              <h3 className="title-label">Host: </h3>
+              <p className="room__lobby-host">{host}</p>
+            </div>
+            <div className="room__lobby-wrapper room__lobby-players-wrapper">
+              <h3 className="room__lobby-players-title title-label">
+                Players:{" "}
+              </h3>
+              <ul className="room__lobby-players-list">
+                {players.map((player, index) => (
+                  <li className="room__lobby-players" key={index}>
+                    {player}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
         <MatchSettings />
